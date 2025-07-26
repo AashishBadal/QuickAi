273bb9747dc9ecdb17457d0afb38e284a9d07d18
AashishBadal <aashishbadal4@gmail.com>
2025-07-26 14:27:53 +0545

    created dashboard,write article and blog titles page ui

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useClerk, useUser } from "@clerk/clerk-react";
+import { Protect, useClerk, useUser } from "@clerk/clerk-react";
 import {
   Eraser,
   FileText,
@@ -6,6 +6,7 @@ import {
   House,
   Icon,
   Image,
+  LogOut,
   Scissors,
   SquarePen,
   Users,
@@ -66,7 +67,11 @@ const Sidebar = ({ sidebar, setSidebar }) => {
       } transition-all duration-300 ease-in-out`}
     >
       <div className="my-7 w-full">
-        <img src={user?.imageUrl} alt="" className="w-13 rounded-full mx-auto" />
+        <img
+          src={user?.imageUrl}
+          alt=""
+          className="w-13 rounded-full mx-auto"
+        />
         <h1 className="mt-1 text-center">{user?.fullName}</h1>
         <div className="px-6 mt-5 text-sm text-gray-600 font-medium">
           {navItems.map((item) => (
@@ -85,13 +90,29 @@ const Sidebar = ({ sidebar, setSidebar }) => {
             >
               {({ isActive }) => (
                 <>
-                  <item.Icon className={`w-4 h-4 ${isActive ? 'text-white' : 'text-gray-600'}`} />
+                  <item.Icon
+                    className={`w-4 h-4 ${
+                      isActive ? "text-white" : "text-gray-600"
+                    }`}
+                  />
                   <span>{item.label}</span>
                 </>
               )}
             </NavLink>
           ))}
         </div>
+      </div>
+      <div className="w-full border-t border-gray-200 p-4 px-7 flex items-center justify-between">
+        <div onClick={openUserProfile} className="flex gap-2 items-center cursor-pointer">
+          <img src={user.imageUrl} alt="" className="w-8 rounded-full" />
+          <div>
+            <h1 className="text-sm font-medium">{user.fullName}</h1>
+            <p className="text-xs text-gray-500">
+              <Protect plan='premium' fallback='Free'>Premium</Protect>Plan
+            </p>
+          </div>
+        </div>
+        <LogOut onClick={signOut} className="w-4.5 text-gray-400 hover:text-gray-700 transition cursor-pointer"/>
       </div>
     </div>
   );
