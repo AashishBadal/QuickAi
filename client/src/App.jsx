0aea3e8ae8e110b7b84458a5031a6968ef243a09
AashishBadal <aashishbadal4@gmail.com>
2025-07-26 11:14:11 +0545

    created home page ui and sidebar for ai tools section

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import ReviewResume from "./pages/ReviewResume";
+import WriteArticle from "./pages/WriteArticle";
+import Community from "./pages/Community";
+import Dashboard from "./pages/Dashboard";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import Layout from "./pages/Layout";
+import BlogTitles from "./pages/BlogTitles";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
